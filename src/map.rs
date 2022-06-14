94b221b38f8406849fae3f9f80f61264f4a4cdb8
shlocko <mcdude22@gmail.com>
2022-06-14 14:25:17 -0700

    Added player

--- src/map.rs
+++ src/map.rs
@@ -10,6 +10,7 @@ pub enum TileType {
 pub struct Map {
     pub tiles: Vec<TileType>,
 }
+
 impl Map {
     pub fn new() -> Self {
         Self {
@@ -29,6 +30,21 @@ impl Map {
                     }
                 }
             }
+        }
+    }
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH
+            && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point)
+            && self.tiles[map_idx(point.x, point.y)]==TileType::Floor
+    }
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
         }
     }
 }
